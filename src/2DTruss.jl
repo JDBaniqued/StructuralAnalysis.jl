c4af27821f58e4fcdd767878eb3c0a2c2a798890
JDBaniqued <62052979+JDBaniqued@users.noreply.github.com>
2020-09-25 02:39:50 +0800

    Update 2DTruss.jl

--- src/2DTruss.jl
+++ src/2DTruss.jl
@@ -1,110 +1,43 @@
-### A Pluto.jl notebook ###
-# v0.11.14
+# Truss Finite Element Analysis
 
-using Markdown
-using InteractiveUtils
+using LinearAlgebra
 
-# ╔═╡ e15dd650-fb20-11ea-0430-db6f140f8052
-md"# Truss Finite Element Analysis"
+## Local Stiffness Matrix"
 
-# ╔═╡ 25074f30-fb21-11ea-2287-4d9af37e41fa
-md"## Model Description"
-
-# ╔═╡ 33d91a20-fb21-11ea-0090-d32238c11685
-md"### Nodes"
-
-# ╔═╡ 52b9eaf0-fb21-11ea-1621-470a7d53e6f6
-begin
-	node = Dict{Integer,Array{Float64}}()
-	node[1] = [4000/tan(30*π/180),4000]
-	node[2] = [10928,0]
-	node[3] = [0,0]
-	node
+function L(member::Dict{Integer,Array{Integer}}, node::Dict{Integer,Array{Float64}})
+	l = Dict{Integer, Float64}()
+	for e = 1:length(member)
+		l[e] = hypot(node[member[e][2]][1]-node[member[e][1]][1], node[member[e][2]][2]-node[member[e][1]][2])
+	end
+	return l
 end
 
-# ╔═╡ ccf5e840-fbbd-11ea-033e-059d3630806d
-md"### Members"
+L(member,node)
 
-# ╔═╡ d2451810-fbb4-11ea-2620-9982c7135dc7
-begin
-	member = Dict{Integer,Array{Integer}}()
-	member[1] = [1,2]
-	member[2] = [1,3]
-	member[3] = [2,3]
-	member
+function cosϕ(member::Dict{Integer,Array{Integer}}, node::Dict{Integer,Array{Float64}})
+	cosphi = Dict{Integer, Float64}()
+	for e = 1:length(member)
+		hor = node[member[e][2]][1]-node[member[e][1]][1]
+		hyp = hypot(node[member[e][2]][1]-node[member[e][1]][1], node[member[e][2]][2]-node[member[e][1]][2])
+		cosphi[e] = hor/hyp
+	end
+	return cosphi
 end
 
-# ╔═╡ db18fca0-fbbd-11ea-1dd3-0f6de312c90c
-md"### Modulus of Elasticity"
+cosϕ(member,node)
 
-# ╔═╡ ee8adc90-fbbd-11ea-1717-839320c5edbb
-begin
-	E = Dict{Integer, Float64}()
-	E[1] = 200
-	E[2] = 200
-	E[3] = 200
-	E
+function sinϕ(member::Dict{Integer,Array{Integer}}, node::Dict{Integer,Array{Float64}})
+	sinphi = Dict{Integer, Float64}()
+	for e = 1:length(member)
+		hor = node[member[e][2]][2]-node[member[e][1]][2]
+		hyp = hypot(node[member[e][2]][1]-node[member[e][1]][1], node[member[e][2]][2]-node[member[e][1]][2])
+		sinphi[e] = hor/hyp
+	end
+	return sinphi
 end
 
-# ╔═╡ 183efa80-fbbe-11ea-120f-c91e6a8f883d
-md"### Cross Sectional Area"
+sinϕ(member,node)
 
-# ╔═╡ 248adde0-fbbe-11ea-2190-87ab49de85be
-begin
-	A = Dict{Integer, Float64}()
-	A[1] = 20000
-	A[2] = 15000
-	A[3] = 18000
-	A
-end
-
-# ╔═╡ 16f2b450-fbd2-11ea-30bd-7751c90f730d
-md"### Forces"
-
-# ╔═╡ 297419c0-fbd2-11ea-3fc7-1f200584578c
-begin
-	f = Dict{Integer, Array}()
-	f[1] = [500*cos(40*π/180),500*sin(40*π/180)]
-	f[2] = [0,missing]
-	f[3] = [missing,missing]
-	f
-end
-
-# ╔═╡ b6d185f0-fbd2-11ea-2d92-192bed8ee971
-md"### Displacements"
-
-# ╔═╡ c6346d50-fbd2-11ea-0029-cfd7141e59f1
-begin
-	u = Dict{Integer, Array}()
-	u[1] = [missing,missing]
-	u[2] = [missing,0]
-	u[3] = [0,0]
-	u
-end
-
-# ╔═╡ f2b33ede-fb20-11ea-21d4-5792f5aeb11b
-md"## Local Stiffness Matrix"
-
-# ╔═╡ 1685c720-fb21-11ea-1ce1-310f5e226283
-function L(a::Array{Integer})
-	return hypot(node[a[1]][1]-node[a[2]][1],node[a[1]][2]-node[a[2]][2])
-end
-
-# ╔═╡ df170212-fbb9-11ea-248a-8152e27dfa4a
-function cosϕ(a::Array{Integer})
-	hor = node[a[2]][1]-node[a[1]][1]
-	hyp = hypot(node[a[1]][1]-node[a[2]][1],node[a[1]][2]-node[a[2]][2])
-	return hor/hyp
-end
-
-# ╔═╡ e424c5a0-fbbc-11ea-2a89-8f910bc843d3
-function sinϕ(a::Array{Integer})
-	ver = node[a[2]][2]-node[a[1]][2]
-	hyp = hypot(node[a[1]][1]-node[a[2]][1],node[a[1]][2]-node[a[2]][2])
-	ver/hyp
-end
-
-# ╔═╡ a55ba700-fc28-11ea-0f52-93d14e401382
 function AxialLocalK(E::Dict, A::Dict, Member::Dict)
 	k = Dict{}()
 	for e = 1:length(member)
@@ -117,13 +50,8 @@ function AxialLocalK(E::Dict, A::Dict, Member::Dict)
 	return k
 end
 
-# ╔═╡ ee27e020-fc2d-11ea-0a1b-6704ccef1be5
 k = AxialLocalK(E,A,member)
 
-# ╔═╡ e5c5c640-fbce-11ea-1bda-b553d206003c
-md"## Global Stiffness Matrix"
-
-# ╔═╡ 95c1bf30-fc2a-11ea-371d-7d891001a9ff
 function AxialGlobalK(k::Dict,member::Dict)
 	K = zeros(length(node)*2, length(node)*2)
 	for e = 1:length(member)
@@ -151,16 +79,9 @@ function AxialGlobalK(k::Dict,member::Dict)
 	return K
 end
 
-# ╔═╡ 3f9b1f40-fc2d-11ea-3ea5-5de05b43505f
 K = AxialGlobalK(k,member)
 
-# ╔═╡ 66d04890-fc2f-11ea-3f7d-af3e8fb0c568
-typeof(u)
-
-# ╔═╡ c10995c0-fbd8-11ea-1079-d379ab6eb740
-md"### Determine free nodal directions"
-
-# ╔═╡ a2200a62-fc30-11ea-063e-a5ef1c6260d0
+### Determine free nodal directions
 function FreeNodalDir(u::Dict)
 	Free = Integer[]
 	for i = 1:length(u)
@@ -174,16 +95,9 @@ function FreeNodalDir(u::Dict)
 	return Free
 end
 
-# ╔═╡ c73cecf0-fc30-11ea-2d5a-cf996ba3d2fe
 free = FreeNodalDir(u)
 
-# ╔═╡ 8b517ffe-fc32-11ea-1d4e-071aec9b2fcb
-typeof(free)
-
-# ╔═╡ e6862480-fbd8-11ea-2ccb-b75584838f33
-md"### Augmented Global Stiffness Matrix"
-
-# ╔═╡ 512b7bb0-fc32-11ea-1ebd-61241a27a867
+### Augmented Global Stiffness Matrix
 function AugmentGlobal(Free::Array,K::Array)
 	K_Aug = zeros(length(Free),length(Free))
 	for m = 1:length(Free)
@@ -195,13 +109,9 @@ function AugmentGlobal(Free::Array,K::Array)
 	K_Aug
 end
 
-# ╔═╡ ab200c30-fc32-11ea-3b2b-71c328c69150
 KAug = AugmentGlobal(free,K)
 
-# ╔═╡ 447e5c90-fbd1-11ea-2352-0717d77ffd3a
-md"## Global Force Matrix"
-
-# ╔═╡ 4c7e51e0-fc33-11ea-1628-cb4a385cdb1f
+## Global Force Matrix"
 function GlobalForce(Free::Array,f::Dict)
 	Force = zeros(length(free))
 	for m = 1:length(free)
@@ -215,52 +125,6 @@ function GlobalForce(Free::Array,f::Dict)
 	return Force
 end
 
-# ╔═╡ 8c587340-fc33-11ea-0a0a-edb42cb25639
 F = GlobalForce(free,f)
 
-# ╔═╡ a4d21630-fbdc-11ea-398d-5d59079dbe16
-begin
-	using LinearAlgebra
-	U = inv(KAug) * F
-end
-
-# ╔═╡ 3d4ffa90-fbdc-11ea-17b5-f91d8ad196f2
-md"## Determining the displacements"
-
-# ╔═╡ Cell order:
-# ╟─e15dd650-fb20-11ea-0430-db6f140f8052
-# ╟─25074f30-fb21-11ea-2287-4d9af37e41fa
-# ╟─33d91a20-fb21-11ea-0090-d32238c11685
-# ╠═52b9eaf0-fb21-11ea-1621-470a7d53e6f6
-# ╟─ccf5e840-fbbd-11ea-033e-059d3630806d
-# ╠═d2451810-fbb4-11ea-2620-9982c7135dc7
-# ╟─db18fca0-fbbd-11ea-1dd3-0f6de312c90c
-# ╠═ee8adc90-fbbd-11ea-1717-839320c5edbb
-# ╟─183efa80-fbbe-11ea-120f-c91e6a8f883d
-# ╠═248adde0-fbbe-11ea-2190-87ab49de85be
-# ╟─16f2b450-fbd2-11ea-30bd-7751c90f730d
-# ╠═297419c0-fbd2-11ea-3fc7-1f200584578c
-# ╟─b6d185f0-fbd2-11ea-2d92-192bed8ee971
-# ╠═c6346d50-fbd2-11ea-0029-cfd7141e59f1
-# ╟─f2b33ede-fb20-11ea-21d4-5792f5aeb11b
-# ╟─1685c720-fb21-11ea-1ce1-310f5e226283
-# ╟─df170212-fbb9-11ea-248a-8152e27dfa4a
-# ╟─e424c5a0-fbbc-11ea-2a89-8f910bc843d3
-# ╟─a55ba700-fc28-11ea-0f52-93d14e401382
-# ╠═ee27e020-fc2d-11ea-0a1b-6704ccef1be5
-# ╟─e5c5c640-fbce-11ea-1bda-b553d206003c
-# ╟─95c1bf30-fc2a-11ea-371d-7d891001a9ff
-# ╠═3f9b1f40-fc2d-11ea-3ea5-5de05b43505f
-# ╠═66d04890-fc2f-11ea-3f7d-af3e8fb0c568
-# ╟─c10995c0-fbd8-11ea-1079-d379ab6eb740
-# ╟─a2200a62-fc30-11ea-063e-a5ef1c6260d0
-# ╠═c73cecf0-fc30-11ea-2d5a-cf996ba3d2fe
-# ╠═8b517ffe-fc32-11ea-1d4e-071aec9b2fcb
-# ╟─e6862480-fbd8-11ea-2ccb-b75584838f33
-# ╟─512b7bb0-fc32-11ea-1ebd-61241a27a867
-# ╠═ab200c30-fc32-11ea-3b2b-71c328c69150
-# ╟─447e5c90-fbd1-11ea-2352-0717d77ffd3a
-# ╟─4c7e51e0-fc33-11ea-1628-cb4a385cdb1f
-# ╠═8c587340-fc33-11ea-0a0a-edb42cb25639
-# ╟─3d4ffa90-fbdc-11ea-17b5-f91d8ad196f2
-# ╠═a4d21630-fbdc-11ea-398d-5d59079dbe16
+U = inv(KAug) * F
